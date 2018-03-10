66abe1c2dfff3d66e2480b7291a9c53ba5c5fa80
akinfemi <akinfemi.aluko@gmail.com>
2018-03-10 00:57:54 -0800

    init

--- lab3/client.py
+++ lab3/client.py
@@ -1,36 +1,33 @@
 import zmq
-import sys, threading
+import threading
+import sys
+import time
 
+# ZeroMQ Context
 context = zmq.Context()
 
+# Define the socket using the "Context"
 socket_sub = context.socket(zmq.SUB)
 socket_push = context.socket(zmq.PUSH)
-socket_push.connect("tcp://127.0.0.1:4242")
-socket_sub.connect("tcp://127.0.0.1:4243")
 
-class Server(username, threading.Thread):
-    def __init__(self, username):
-      threading.Thread.__init__(self)
-      self.username = username
-      print('User [%s] Connected to the chat server' %s self.username)
-      
-    
-    def run(self):
-        while(True):
-            message = sub.recv()
-            all = message.decode().split(' ')
-            if(all[0] != self.username):
-                 print("\n"+data[1]+":"+data[2])
+if (len(sys.argv[0:]) > 1):
+    username = sys.argv[1]
+    username = username.upper()
+else:
+    sys.exit()
+
+# Define subscription and messages with prefix to accept.
+socket_sub.setsockopt_string(zmq.SUBSCRIBE, username)
+socket_sub.connect("tcp://127.0.0.1:4243") #data
+socket_push.connect("tcp://127.0.0.1:4242") #commands
+
+def init(user):
+    print ("User [%s] Connected to server." % user)
+    socket_push.send_string("User [%s] Connected to server." % user)
+
+init(username)
 
 while (True):
-    message=input("["%s"] > " % name)
-    data={'name':name,'message':message}
-    push.send_json(data)
-
-if __name__ == '__main__' :
-    if len(sys.argv[0:]) < 2:
-        print("Usage: python3 client.py <UserName>")
-        sys.exit()
-    else:
-        username = sys.argv[1]
-        server = Server(username)
+    message = input("[%s] > " % username)
+    message = "%s %s" % (username, message)
+    socket_push.send(message.encode('ascii'))
